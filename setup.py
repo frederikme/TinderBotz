b6bc5a0ae2b69784e486ccb067486113db1c8779
Frederik <60892381+frederikme@users.noreply.github.com>
2020-11-20 11:41:02 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
   author='Frederik Mees',                   # Type in your name
   author_email='frederik.mees@gmail.com',      # Type in your E-Mail
   url='https://github.com/frederikme/TinderBot',   # Provide either the link to your github or to your website
-  download_url='',    # I explain this later on
+  download_url='https://github.com/frederikme/TinderBot/archive/0.1.tar.gz',    # I explain this later on
   keywords=['Tinder', 'Automation', 'bot'],   # Keywords that define your project best
   install_requires=[],            # I get to this in a second
 
