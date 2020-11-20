cf15833d4ca77175b07212871c6e34c5c05d26ce
Frederik Mees <Fredjemees@gmail.com>
2020-11-20 12:27:26 +0100

    renaming for pip-package

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
+import setuptools
 from distutils.core import setup
 
 setup(
   name='tinderbotz',         # How you named your loadingz folder (MyLib)
   packages=['tinderbotz'],   # Chose the same as "name"
-  version='0.1',      # Start with a small number and increase it with every change you make
+  version='0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description='Tool for displaying loading processes',   # Give a short description about your library
   author='Frederik Mees',                   # Type in your name
   author_email='frederik.mees@gmail.com',      # Type in your E-Mail
   url='https://github.com/frederikme/TinderBot',   # Provide either the link to your github or to your website
-  download_url='https://github.com/frederikme/TinderBot/archive/0.1.tar.gz',    # I explain this later on
+  download_url='https://github.com/frederikme/TinderBot/archive/0.2.tar.gz',    # I explain this later on
   keywords=['Tinder', 'Automation', 'bot'],   # Keywords that define your project best
-  install_requires=[],            # I get to this in a second
+  install_requires=['selenium', 'webdriver-manager', 'pyfiglet', 'pillow', 'pyglet', 'scipy'],            # I get to this in a second
+
+  package_data={'tinderbotz': ['LocationGuardExtension.crx']},
 
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your loadingz
---
+++ tinderbotz/__init__.py
@@ -0,0 +1,13 @@
+from tinderbotz.session import Session
+
+from tinderbotz.helpers.location_helper import LocationHelper
+from tinderbotz.helpers.profile_helper import ProfileHelper
+
+from tinderbotz.helpers.geomatch import Geomatch
+from tinderbotz.helpers.match import Match
+from tinderbotz.helpers.geomatch_helper import GeomatchHelper
+from tinderbotz.helpers.match_helper import MatchHelper
+from tinderbotz.helpers.login_helper import LoginHelper
+from tinderbotz.helpers.storage_helper import StorageHelper
+from tinderbotz.helpers.loadingbar import LoadingBar
+
