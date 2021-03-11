fd93df7631be26479250cf9334c1ce74b354d8d5
Frederik <60892381+frederikme@users.noreply.github.com>
2021-03-11 15:10:17 +0100

    Update constants_helper.py

--- tinderbotz/helpers/constants_helper.py
+++ tinderbotz/helpers/constants_helper.py
@@ -55,3 +55,32 @@ class Language(enum.Enum):
     TURKISH = "Turkish"
     UKRAINIAN = "Ukrainian"
     VIETNAMESE = "Vietnamese"
+   
+class Printouts(enum.Enum):
+    BANNER = ''' 
+         _____ _           _           _           _       
+        |_   _(_)_ __   __| | ___ _ __| |__   ___ | |_ ____
+          | | | | '_ \ / _` |/ _ \ '__| '_ \ / _ \| __|_  /
+          | | | | | | | (_| |  __/ |  | |_) | (_) | |_ / / 
+          |_| |_|_| |_|\__,_|\___|_|  |_.__/ \___/ \__/___|
+        ----------------------------------------------------'''
+    
+    EXPLANATION = '''
+Hi guys,
+
+This code is opensource and available on GitHub.
+repository: https://github.com/frederikme/TinderBotz
+
+If you find the code useful, it would mean a lot if you can star the repository to show your appreciation.
+If you're interested in learning how to write these bots yourself,
+I will be making tutorials about python selenium automation soon.
+
+youtube_channel: https://www.youtube.com/channel/UC1i3N9R9XYxt5Imi-auLPuA
+tutorials that will be coming:
+1. Scraping news on websites -> For absolute beginners, as an intro to selenium 
+2. A simplified Tinderbot -> For beginners
+3. Writing an automated chess bot to play on Chess.com using stockfish (currently a private repository). -> Advanced
+
+Have a nice day,
+Frederikme
+'''
