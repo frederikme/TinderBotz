43aaf5875ef6b48916fe0f487f6566c157aaadae
Frederik Mees <frederik.mees@gmail.com>
2021-06-05 14:22:09 +0200

    Pypi prep pip release

--- tinderbotz/__init__.py
+++ tinderbotz/__init__.py
@@ -11,3 +11,4 @@ from tinderbotz.helpers.storage_helper import StorageHelper
 from tinderbotz.helpers.loadingbar import LoadingBar
 
 from tinderbotz.helpers.constants_helper import *
+from tinderbotz.helpers.xpaths import *
